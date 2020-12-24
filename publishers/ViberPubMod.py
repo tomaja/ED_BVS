0ca38f2e3661ed4de65679b2e77dc49a99df336e
Dragan Knežević <dragan.knezevic@professionaltrading.com>
2020-12-24 19:13:56 +0100

    Viber was stopping after one failed message. Fixed.

--- publishers/ViberPubMod.py
+++ publishers/ViberPubMod.py
@@ -60,19 +60,19 @@ class ViberPub:
         return res
 
     def Publish(self, message) -> None:
-        try:
-            print('Posting to Viber...' + self.FormatMessage(message.message))
-            UserQ = Query()
-            for user in self.usersDb.search(UserQ.active == '1'):
-                messageCopy = message
-                messageCopy.userId = user['id']
-                if len(self.db.search((self.query.hash == messageCopy.hash) & (self.query.userId == user['id']))) == 0:
+        print('Posting to Viber...' + self.FormatMessage(message.message))
+        UserQ = Query()
+        for user in self.usersDb.search(UserQ.active == '1'):
+            messageCopy = message
+            messageCopy.userId = user['id']
+            if len(self.db.search((self.query.hash == messageCopy.hash) & (self.query.userId == user['id']))) == 0:
+                try:
                     self.viber.send_messages(user['id'], [ TextMessage(text=self.FormatMessage(messageCopy.message)) ])
                     self.db.insert(messageCopy.ToDict())
                     print('Message sent to Viber user: ' + user['name'])
-                else:
-                    print('User ' + user['name'] + ' already notified through Viber')
-        except:
-            print('Posting to Viber failed')
+                except Exception as e:
+                    print('Posting to Viber user {0} -> {1}'.format(user['name'], e))
+            else:
+                print('User ' + user['name'] + ' already notified through Viber')
         
 
