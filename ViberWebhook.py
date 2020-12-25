62b46912dd66834b28c01689df65a945737978ac
Dragan Knežević <dragan.knezevic@professionaltrading.com>
2020-12-25 16:23:48 +0100

    Typo

--- ViberWebhook.py
+++ ViberWebhook.py
@@ -82,7 +82,7 @@ class ViberWebhook:
                 self.usersDb.update({'active': '0'}, UserQ.id == viber_request.user.id)
         elif isinstance(viber_request, ViberSubscribedRequest):
             UserQ = Query()
-            self.viber.send_messages(viber_request.user.id, [ TextMessage(text='Za prijavu pošaljite bilo kakvu poruku.) ])
+            self.viber.send_messages(viber_request.user.id, [ TextMessage(text='Za prijavu pošaljite bilo kakvu poruku.') ])
             if len(self.usersDb.search(UserQ.id == viber_request.user.id)) == 0:
                 self.usersDb.insert({'id': viber_request.user.id, 'name': viber_request.user.name, 'active': '1'})
             else:
